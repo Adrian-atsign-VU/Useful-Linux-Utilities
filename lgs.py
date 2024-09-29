95ddaf79c3b123ffb71319389c95b50b217faac1
Meme Guy <owen.debiasio@gmail.com>
2024-09-29 19:13:31 -0400

    Update lgs.py

--- lgs.py
+++ lgs.py
@@ -12,54 +12,67 @@ def invalidinput():
 
 if ostype == 'Linux':
 
-    print("Linux Gaming Setup Utility v0.1\n\n")
+    print("Linux Gaming Setup Utility v0.1.5\n\n")
 
     #Install Steam ********************************************************************************************************
-    response = input("Would you like to install steam?\n")
+        
+    print("Is Steam installed?\nY or N\n")    
+    response = input("")
 
     if response == 'Y':
-        print("Do you want to install the System Package or Flatpak?\n")
-        response = input("")
-        if response == 'Flatpak':
-            subprocess.run(['sudo flatpak install steam'], shell = True)
-            
-            askforcustomproton = 1
-        elif response == 'System Package':
-            subprocess.run(['sudo apt install steam'], shell = True)
-            askforcustomproton = 1
-        else:
-            invalidinput()
-
-    elif response == 'N':
-        askforcustomproton = 0
-    else:
-        askforcustomproton = 0
-
-    if exitenabled == 0:
-        if askforcustomproton == 1:
-            print("Would you like to install the custom proton (GE Proton)?\n")
+        response = input("\nWould you like to install steam?\nY or N\n")
+        
+        if response == 'Y':
+            print("\nDo you want to install the System Package or Flatpak?\nY or N\n")
             response = input("")
+            if response == 'Flatpak':
+                subprocess.run(['sudo flatpak install steam'], shell = True)
+                
+                askforcustomproton = 1
+            elif response == 'System Package':
+                subprocess.run(['sudo apt install steam'], shell = True)
+                askforcustomproton = 1
+            else:
+                invalidinput()
 
-            if response == 'Y':
-                print("Are you using the Flatpak version of Steam?")
+        elif response == 'N':
+            askforcustomproton = 0
+        else:
+            askforcustomproton = 0
+
+        if exitenabled == 0:
+            if askforcustomproton == 1:
+                print("Would you like to install the custom proton (GE Proton)?\nY or N\n")
                 response = input("")
 
                 if response == 'Y':
-                    subprocess.run(['wget https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton9-15/GE-Proton9-15.tar.gz'], shell = True)
-                    subprocess.run(['tar -xf GE-Proton*.tar.gz -C ~/.var/app/com.valvesoftware.Steam/data/Steam/compatibilitytools.d/'], shell = True)
+                    print("Are you using the Flatpak version of Steam?\nY or N\n")
+                    response = input("")
 
-                elif response == 'N':
-                    subprocess.run(['wget https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton9-15/GE-Proton9-15.tar.gz'], shell = True)
-                    subprocess.run(['mkdir ~/.steam/root/compatibilitytools.d'], shell = True)
-                    subprocess.run(['cd ~/Downloads'], shell = True)
-                    subprocess.run(['tar -xf GE-Proton*.tar.gz -C ~/.steam/root/compatibilitytools.d/'], shell = True)
-                    
-                
-                else:
-                    invalidinput()
+                    if response == 'Y':
+                        subprocess.run(['wget https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton9-15/GE-Proton9-15.tar.gz'], shell = True)
+                        subprocess.run(['tar -xf GE-Proton*.tar.gz -C ~/.var/app/com.valvesoftware.Steam/data/Steam/compatibilitytools.d/'], shell = True)
 
-    print("You will most likely need to restart steam manually.\nPress enter to continue.\n")
-    input()
+                    elif response == 'N':
+                        subprocess.run(['wget https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton9-15/GE-Proton9-15.tar.gz'], shell = True)
+                        subprocess.run(['mkdir ~/.steam/root/compatibilitytools.d'], shell = True)
+                        subprocess.run(['cd ~/Downloads'], shell = True)
+                        subprocess.run(['tar -xf GE-Proton*.tar.gz -C ~/.steam/root/compatibilitytools.d/'], shell = True)
+                        
+                    else:
+                        invalidinput()
+
+        print("You will most likely need to restart steam manually.\nPress enter to continue.\n")
+        input()
+
+    print("Do you want to install Wine?\nY or N\n")
+    response = input("")
+
+    if response == 'Y':
+        subprocess.run(['sudo apt install wine'], shell = True)
+
+        print("\nend")
+
 else:
     print("This script is for Linux machines only, hence the name.")
 
