b4de801965c454124fa4b9043d08f8765dc84da3
Meme Guy <owen.debiasio@gmail.com>
2024-10-26 11:42:06 -0400

    Add files via upload

---
+++ Linux Mint debloater.sh
@@ -0,0 +1,21 @@
+sudo apt update
+
+apt remove mintchat -f #Matrix
+
+apt remove uno-libs-private -f #libreoffice
+
+apt remove hypnotix -f
+
+apt remove thingy -f # Library
+
+apt remove gnome-calendar -f # calendar
+
+apt remove thunderbird -f
+
+apt remove transmission-gtk -f # transmission
+
+apt remove pix -f
+
+apt remove simple-scan -f #document scanner
+
+apt remove rhythmbox -f
